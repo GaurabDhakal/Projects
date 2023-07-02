18016a1e66bf6cabf27c9cfd5df69718356bd96f
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2023-07-02 19:44:43 +0545

    fixed few things and patched output area after each function call

--- JS/ytdr.js
+++ JS/ytdr.js
@@ -3,6 +3,7 @@ const outputArea = document.getElementById('output')
 const loader  = document.getElementById('loadingGif');
 
 let dislikeReturn = async () => {
+    outputArea.innerHTML =""
     loader.hidden = false;
     const usrVal = document.getElementById('name').value;
 let options = {
@@ -16,6 +17,9 @@ let options = {
     }
 let response = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${usrVal}`,options);
 let result = await response.json();
+const keysCount = Object.keys(result);
+console.log(keysCount);
+if(keysCount.length==7){
 let chkStatus = ()=>{
     if((result.deleted)===true){
         return "This video is deleted!"
@@ -31,3 +35,14 @@ outputArea.innerHTML = `<p id="warning">ID of the video : ${result.id} <br>
                         Views Count: ${result.viewCount} <br>
                         Video Status: ${chkStatus()}<p>`
 }
+                    else{
+                        loader.hidden=true;
+                        outputArea.innerHTML=`Video with the id ${usrVal} doesn't exist!`
+                    }
+}
+
+document.getElementById('name').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        resultCheck();
+    }
+});
