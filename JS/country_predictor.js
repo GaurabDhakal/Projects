99fd2a2349603311c31c5b32069cc50364da6de9
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-01-03 20:52:23 +0545

    history area design changes

--- JS/country_predictor.js
+++ JS/country_predictor.js
@@ -4,7 +4,7 @@ const output = document.getElementById('output');
 const historyWarning = document.getElementById('historyWarning');
 const loadingArea = document.querySelector('.hideLoadingGif');
 let clearHistoryBtnElem = document.getElementById("clearHistory");
-
+historyWarning.hidden=false;
 let countryPredict= async () =>{
     let usrInpElem = document.getElementById('country')
     let usrInp = usrInpElem.value;
@@ -50,7 +50,6 @@ let countryPredict= async () =>{
 }
 document.getElementById('country').addEventListener('keyup',  (event)=> {
     if (event.key === 'Enter') {
-
         countryPredict();
     }
 });
@@ -66,20 +65,23 @@ function showHistory() {
     historyDisplay.hidden = false;
     let keys = Object.keys(localStorage);
     if(keys.length===0){
+    historyWarning.hidden=false;
+    historyWarning.innerHTML = "No record found!"
     clearHistoryBtnElem.hidden=true;
     }else if(keys.length>0){
         clearHistoryBtnElem.hidden =false;
     }
-    let i = 1;
         for (let key of keys) {
             
             let value = localStorage.getItem(key);
             try {
+                historyDisplayArea.hidden=false;
                 let parsedValue = JSON.parse(value);
                 if (typeof parsedValue === "object" && parsedValue !== null && !Array.isArray(parsedValue)) {
-                    historyWarning.innerHTML = ""
-                    historyDisplayArea.innerHTML += `<p>${i}. ${key} ` + ": " + countryName.of(parsedValue.country[0].country_id) + `, Probability: ${parsedValue.country[0].probability}</p><br>`;
-                    i++;
+                    historyWarning.hidden=true;
+                    const name = key.charAt(0).toUpperCase() + key.slice(1)
+                    historyDisplayArea.innerHTML += `<p id="toBeHiddenLater"> ${name} ` + ": " + countryName.of(parsedValue.country[0].country_id) + `, Probability: ${parsedValue.country[0].probability}</p><br>`;
+                    
                 }
             } catch (error) {
                 console.error("Error parsing value for key:", key, error);
@@ -98,6 +100,7 @@ function goBack() {
     historyDisplayArea.innerHTML = "";
     output.innerHTML = ""
     if (keys.length < 3) {
+        historyWarning.hidden=false;
         historyWarning.innerHTML = "No recorded history found!!";
     }
 }
@@ -105,6 +108,8 @@ function goBack() {
 //Clear History Function
 function clearHistory() {
     const keysForClear = Object.keys(localStorage);
+    historyWarning.hidden=false;
+    historyDisplayArea.hidden=true;
     console.log("clicked");
     if(keysForClear.length === 0){
         historyWarning.innerHTML="History is already empty."
@@ -120,3 +125,21 @@ function clearHistory() {
         historyWarning.innerHTML = "History has been successfully cleared!";
     }
 }
+
+// Shift+Enter for Show History Function
+
+document.getElementById("country").addEventListener("keydown",(e)=>{
+        if(e.shiftKey &&e.key === "Enter"){
+            showHistory();
+        }
+})
+let usrInpElem = document.getElementById('country');
+document.addEventListener('DOMContentLoaded', function () {
+usrInpElem.addEventListener("input",()=>{
+    if(usrInpElem.value.length>0){
+        if(usrInpElem.classList.contains("invalidInputWarning")){
+            usrInpElem.classList.remove("invalidInputWarning")
+        }
+    }
+})
+})
