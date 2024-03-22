1132da77968c790938f46882a3ee4cac0a6775ed
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-03-22 10:58:15 +0545

    Update handleNoListMsg.js and index.html

--- TODOLIST/JS/handleNoListMsg.js
+++ TODOLIST/JS/handleNoListMsg.js
@@ -3,7 +3,7 @@ let wellWishes = [
     "Clock out time! Wishing you a fun-filled rest of the day.",
     "Tasks complete! Go recharge and do something you love.",
     "Well done! Now go make the most of your free time.",
-    "That's it for work tasks. Time to switch to relax mode!",
+    "That's it for work tasks! Time to switch to relax mode!",
     "Step away from the desk! Have a delightful rest of your day.",
     "Awesome job today! May your evening be equally fantastic.",
     "High five for finishing! Go out and seize the rest of your day.",
