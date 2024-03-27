e7609c4faae6e63a7723955a0cd195da7d5450f7
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-03-27 16:01:30 +0545

    Fixed the active page href issue

--- JS/navloader.js
+++ JS/navloader.js
@@ -1,22 +1,19 @@
 function checkActive(attribute,winAddress){
-if(winAddress.endsWith(attribute)){
-    return true;
-  }
+    console.log(winAddress)
+    if(winAddress.endsWith(attribute)||winAddress=="/"&&attribute=="index.html"){
+        return true;
+      }
 }
-  
   function buildHTML(elementData) {
       const element = document.createElement(elementData.tagName);
       for (const attribute in elementData.attributes) {
-        if (elementData.tagName == 'a'&&checkActive(elementData.attributes.href , window.location.pathname)) {
+        if (elementData.tagName == 'a'&&checkActive(elementData.attributes.href, window.location.pathname)) {
             element.classList.add('active');
         }
         if (attribute === 'class') {
           element.classList.add(...elementData.attributes[attribute].split(' ')); 
         } 
-        else if(attribute=='href'&&!element.classList.contains("active")){
-          console.log(element.classList.contains("active"))
-          element.setAttribute(attribute,elementData.attributes[attribute]);
-        }else{
+        else{
           element.setAttribute(attribute, elementData.attributes[attribute]);
       }
       }
@@ -40,7 +37,11 @@ async function fetchAndParseJSON() {
   const response = await fetch('./JS/navbar.json');
   const data = await response.json();
   const navigation = buildHTML(data[0]); 
-  console.log(data)
-  navbarContainer.appendChild(navigation); 
+  navbarContainer.appendChild(navigation);
+  const elem = document.querySelector(".active")
+  elem.href = "#"
+  console.log(elem)
 }
 fetchAndParseJSON();
+
+
