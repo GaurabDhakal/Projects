fa903ca24f33b257e8bc0b5f9e359a07960e366a
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-04-07 21:19:33 +0545

    small fixes

--- TODOLIST/JS/logout.js
+++ TODOLIST/JS/logout.js
@@ -4,7 +4,10 @@ function logout(){
     let keys = Object.keys(localStorage).filter(key=>key.startsWith("todo_"));
     let keysOfCategory = Object.keys(localStorage).filter(key=>key.startsWith('CATE_'));
     let username = "nameOfUsr"
+    let editHistoryKeys = Object.keys(localStorage).filter(key=>key.startsWith("edithistory"));
+    editHistoryKeys.forEach(key => localStorage.removeItem(key));
     keysOfCategory.forEach(key => localStorage.removeItem(key));
+    localStorage.removeItem("todoList_todaysData");
     keys.forEach(key=>localStorage.removeItem(key));
     localStorage.removeItem(username);
     window.location.reload();
