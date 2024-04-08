87256d9f514ee31bf20f3d90d7ba0f0b39387d99
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-04-09 01:22:21 +0545

    Update CSS styles for TODO list and TODO expand

--- JS/country_predictor.js
+++ JS/country_predictor.js
@@ -1,121 +1,139 @@
 let keys = Object.keys(localStorage);
-const countryName = new Intl.DisplayNames(['EN'], { type: 'region' });
-const output = document.getElementById('output');
-const historyWarning = document.getElementById('historyWarning');
+const countryName = new Intl.DisplayNames(["EN"], { type: "region" });
+const output = document.getElementById("output");
+const historyWarning = document.getElementById("historyWarning");
 let clearHistoryBtnElem = document.getElementById("clearHistory");
 const currentDate = new Date();
 const userTimezoneOffset = currentDate.getTimezoneOffset(); // Get the user's timezone offset in minutes
-const userTimestamp = new Date(currentDate.getTime() - userTimezoneOffset * 60000);
+const userTimestamp = new Date(
+    currentDate.getTime() - userTimezoneOffset * 60000
+);
 const prefixKey = "CP_";
 
-//Checks teh type of error
 let universalResult;
-let chkError = ()=>{
-    if(universalResult.error){
-        return universalResult.error;
-    }else{
-        return "Data of name isn't available!"
-    }
+let chkError = () => {
+        return "Data of name isn't available!";
+
+};
+
+// Function to display error message in output element
+function displayErrorMessage(message) {
+    document.querySelector(".errorArea").textContent = message;
 }
 
-let loadingArea = document.getElementById("loadingGif");
-let countryPredict= async () =>{
-    let usrInpElem = document.getElementById('country')
-    let usrInp = usrInpElem.value;
-    let hasSpace  = usrInp.includes(" ");
-    loadingArea.hidden=false;
+// Function to display loading message in output element
+function displayLoadingMessage() {
+    document.querySelector(".errorArea").textContent = "Loading";
+}
 
-    if (usrInp.length === 0) {
-        loadingArea.hidden=true;
-        usrInpElem.classList.add("invalidInputWarning")
-        output.innerHTML = `<p class="invalidInputWarning invalidInputWarningPtag">Kindly enter the name first!</p>`
-    } else if(hasSpace){
-        loadingArea.hidden=true;
-        output.innerHTML = `<p class="invalidInputWarningPtag invalidInputWarning">Whitespaces are not allowed(first name only)</p>`
-    } 
-    else {
-        if(usrInpElem.classList.contains("invalidInputWarning")){
-            usrInpElem.classList.remove("invalidInputWarning")
-        }
-        output.innerHTML = "Loading";
-        let options = {
-            method: 'GET'
-        }
-        try{
-        let response = await fetch(`https://api.nationalize.io?name=${usrInp}`, options);
-        let result = await response.json();
-        universalResult = result;
-        const dataToStore = {
-            data: result,
-            timestamp: new Date().toISOString(), // Current timestamp in ISO format
-        };
-        
-          // Ensure 'country' property exists before adding it to the stored data
-        if (result.country && Array.isArray(result.country) && result.country.length > 0) {
-            dataToStore.country = result.country;
-        }
-        if(result.count!=0){
-        localStorage.setItem(prefixKey+usrInp, JSON.stringify(dataToStore));
-        }
-        if(result.country === null){
-            output.innerHTML = "Data not available";
-            console.log("no data")
-        }else{
-            output.innerHTML = "";
-            loadingArea.hidden=true;
-            let resultElem = document.createElement("table");
-            output.append(resultElem);
-            resultElem.innerHTML = `<tr>
-            <th>S.N</td>
-            <th>Country</th>
-            <th>Probability</th>
-            </tr>`
-            for (let i = 0; i < 5; i++) {
-                let countryCode = await result.country[i].country_id;
-                
-                resultElem.innerHTML += `<tr>
+function displayResult(result) {
+    document.querySelector(".errorArea").hidden = true;
+    // Show the output div
+    output.hidden = false;
+    // Hide the loading area
+    loadingArea.hidden = true;
+
+    // Select the table body element within the output div
+    const tableBody = output.querySelector("tbody");
+
+    // Clear previous table rows
+    tableBody.innerHTML = "";
+
+    // Iterate over the result data and dynamically add table rows
+    for (let i = 0; i < 5 && i < result.country.length; i++) {
+        let countryCode = result.country[i].country_id;
+        let row = `
+            <tr>
                 <td>${i + 1}</td> 
                 <td>${countryName.of(countryCode)}</td>
-                <td>${result.country[i].probability}</td>
-                </tr
-                >`;
-            }
-        }
-        
-    }catch(error){
-        loadingArea.hidden=true;
-        output.innerHTML = `<div id="oa">${chkError()}
-        </div>`;
-    }
+                <td>${result.country[i].probability.toFixed(3)}</td>
+            </tr>
+        `;
+        tableBody.insertAdjacentHTML("beforeend", row);
     }
 }
-document.getElementById('country').addEventListener('keyup',  (event)=> {
-    if (event.key === 'Enter') {
+let loadingArea = document.getElementById("loadingGif");
+async function countryPredict() {
+    let usrInpElem = document.getElementById("country");
+    let usrInp = usrInpElem.value;
+    output.hidden = true;
+    let hasSpace = usrInp.includes(" ");
+    loadingArea.hidden = false;
+
+    if (usrInp.length === 0) {
+        loadingArea.hidden = true;
+        usrInpElem.classList.add("invalidInputWarning");
+        displayErrorMessage("Kindly enter the name first!");
+    } else if (hasSpace) {
+        loadingArea.hidden = true;
+        displayErrorMessage("Whitespaces are not allowed (first name only)");
+    } else {
+        if (usrInpElem.classList.contains("invalidInputWarning")) {
+            usrInpElem.classList.remove("invalidInputWarning");
+        }
+        displayLoadingMessage();
+        let options = {
+            method: "GET",
+        };
+        try {
+            let response = await fetch(`https://api.nationalize.io?name=${usrInp}`, options);
+            let result = await response.json();
+            universalResult = result;
+            console.log(result)
+            const dataToStore = {
+                data: result,
+                timestamp: new Date().toISOString(), // Current timestamp in ISO format
+            };
+
+            // Ensure 'country' property exists before adding it to the stored data
+            if (result.country && Array.isArray(result.country) && result.country.length > 0) {
+                dataToStore.country = result.country;
+            }
+            if (result.country === null) {
+                displayErrorMessage("Data not available");
+            } else if(result.count>0) {
+                localStorage.setItem(prefixKey + usrInp, JSON.stringify(dataToStore));
+                displayResult(result);
+            }else{
+                loadingArea.hidden = true;
+                console.log("No data found")
+                displayErrorMessage(chkError());
+            }
+        } catch (error) {
+            loadingArea.hidden = true;
+            displayErrorMessage(`Error: ${error}`);
+        }
+    }
+}
+
+// Add event listener for the "Enter" key
+document.getElementById("country").addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
         countryPredict();
     }
 });
 
-
 const container = document.getElementById("containerOuterlayer");
 const historyDisplayArea = document.getElementById("historyDisplayArea");
-const historyDisplay = document.getElementById("history");
+const historyDisplay = document.getElementById("historyParent");
+const historyDisplayChild = document.getElementById("history");
 const footer = document.getElementById("footer");
-//show history function
+
 function showHistory() {
-    footer.hidden=true
+    footer.hidden = true;
     container.hidden = true;
     historyDisplay.hidden = false;
     let keys = Object.keys(localStorage);
-    let countOfOthers = 0
+    let countOfOthers = 0;
     let countToCheck = 0;
-    for(let key of keys){
-        if(key.startsWith("CP_")){
+    for (let key of keys) {
+        if (key.startsWith("CP_")) {
             countToCheck++;
-        }else{
+        } else {
             countOfOthers++;
         }
     }
-    if (countToCheck<=0) {
+    if (countToCheck <= 0) {
         historyWarning.innerHTML = `<p class="noRF">No record found!</p>`;
         historyWarning.hidden = false;
         clearHistoryBtnElem.hidden = true;
@@ -124,35 +142,51 @@ function showHistory() {
     }
 
     keys.sort((a, b) => {
-        if(a.startsWith("CP_")&&b.startsWith("CP_")){
-        return new Date(JSON.parse(localStorage.getItem(b)).timestamp) - new Date(JSON.parse(localStorage.getItem(a)).timestamp);
-    }
+        if (a.startsWith("CP_") && b.startsWith("CP_")) {
+            return (
+                new Date(JSON.parse(localStorage.getItem(b)).timestamp) -
+                new Date(JSON.parse(localStorage.getItem(a)).timestamp)
+            );
+        }
     });
 
     for (let key of keys) {
-        if(key.startsWith("CP_")){
-        let value = localStorage.getItem(key);
-        try {
-            historyDisplayArea.hidden = false;
-            let parsedValue = JSON.parse(value);
-            if (typeof parsedValue === "object" && parsedValue !== null && !Array.isArray(parsedValue)&&parsedValue.data.count!=0&&key.startsWith("CP_")) {
-                const timestamp = new Date(parsedValue.timestamp).toLocaleDateString('en-CA', {
-                    year: 'numeric',
-                    month: '2-digit',
-                    day: '2-digit'
-                }).replace(/\//g, '-');
-                historyWarning.hidden = true;
-                
-                const name = key.charAt(3).toUpperCase() + key.slice(4);
-                historyDisplayArea.innerHTML += `<div id="toBeHiddenLater"> ${timestamp}: ${name} ` + ": " + countryName.of(parsedValue.country[0].country_id) + `, Probability: ${parsedValue.country[0].probability}</div><br>`;
+        if (key.startsWith("CP_")) {
+            let value = localStorage.getItem(key);
+            try {
+                historyDisplayArea.hidden = false;
+                let parsedValue = JSON.parse(value);
+                if (
+                    typeof parsedValue === "object" &&
+                    parsedValue !== null &&
+                    !Array.isArray(parsedValue) &&
+                    parsedValue.data.count != 0 &&
+                    key.startsWith("CP_")
+                ) {
+                    const timestamp = new Date(parsedValue.timestamp)
+                        .toLocaleDateString("en-CA", {
+                            year: "numeric",
+                            month: "2-digit",
+                            day: "2-digit",
+                        })
+                        .replace(/\//g, "-");
+                    historyWarning.hidden = true;
+
+                    const name = key.charAt(3).toUpperCase() + key.slice(4);
+                    historyDisplayArea.innerHTML +=
+                        `<div id="toBeHiddenLater"> ${timestamp}: ${name} ` +
+                        ": " +
+                        countryName.of(parsedValue.country[0].country_id) +
+                        `, Probability: ${parsedValue.country[0].probability.toFixed(
+                            3
+                        )}</div><br>`;
+                }
+            } catch (error) {
+                console.error("Error parsing value for key:", key, error);
             }
-        } catch (error) {
-            console.error("Error parsing value for key:", key, error);
         }
     }
-    }
 }
-
 
 function isValidFormat(parsedValue) {
     return (
@@ -160,66 +194,59 @@ function isValidFormat(parsedValue) {
         parsedValue !== null &&
         !Array.isArray(parsedValue) &&
         parsedValue.timestamp &&
-        typeof parsedValue.timestamp === 'string'
+        typeof parsedValue.timestamp === "string"
     );
 }
 
-
-// Go back function
 function goBack() {
     container.hidden = false;
-    footer.hidden=false;
+    footer.hidden = false;
     historyDisplay.hidden = true;
     historyDisplayArea.innerHTML = "";
-    output.innerHTML = ""
+    output.hidden = true;
     if (keys.length < 3) {
-        historyWarning.hidden=false;
+        historyWarning.hidden = false;
         historyWarning.innerHTML = "No recorded history found!!";
     }
 }
 
-//Clear History Function
 function clearHistory() {
     const keysForClear = Object.keys(localStorage);
-    historyWarning.hidden=false;
-    historyDisplayArea.hidden=true;
+    historyWarning.hidden = false;
+    historyDisplayArea.hidden = true;
     console.log("cleared");
-    if(keysForClear.length === 0){
-        historyWarning.innerHTML="History is already empty."
-    }
-    else if (keysForClear.length > 0) {
+    if (keysForClear.length === 0) {
+        historyWarning.innerHTML = "History is already empty.";
+    } else if (keysForClear.length > 0) {
         let keys = Object.keys(localStorage);
-        for(let key of keys){
-            if(key.startsWith("CP_")){
+        for (let key of keys) {
+            if (key.startsWith("CP_")) {
                 localStorage.removeItem(key);
             }
         }
         historyDisplayArea.innerHTML = "";
 
-        clearHistoryBtnElem.textContent = "Clearing.."
-        setTimeout(()=>{
-            clearHistoryBtnElem.textContent = "Clear History"
+        clearHistoryBtnElem.textContent = "Clearing..";
+        setTimeout(() => {
+            clearHistoryBtnElem.textContent = "Clear History";
             clearHistoryBtnElem.hidden = true;
-        },400)
+        }, 400);
         historyWarning.innerHTML = "History has been successfully cleared!";
     }
 }
 
-// Shift+Enter for Show History Function
-
-document.getElementById("country").addEventListener("keydown",(e)=>{
-        if(e.shiftKey &&e.key === "Enter"){
-            showHistory();
-        }
-})
-let usrInpElem = document.getElementById('country');
-document.addEventListener('DOMContentLoaded', function () {
-usrInpElem.addEventListener("input",()=>{
-    if(usrInpElem.value.length>0){
-        if(usrInpElem.classList.contains("invalidInputWarning")){
-            usrInpElem.classList.remove("invalidInputWarning")
-        }
-        
+document.getElementById("country").addEventListener("keydown", (e) => {
+    if (e.shiftKey && e.key === "Enter") {
+        showHistory();
     }
-})
-})
+});
+let usrInpElem = document.getElementById("country");
+document.addEventListener("DOMContentLoaded", function () {
+    usrInpElem.addEventListener("input", () => {
+        if (usrInpElem.value.length > 0) {
+            if (usrInpElem.classList.contains("invalidInputWarning")) {
+                usrInpElem.classList.remove("invalidInputWarning");
+            }
+        }
+    });
+});
