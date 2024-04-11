191ef27091ab3e2705de9242ef68b859b8f1b73c
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2024-04-11 23:19:39 +0545

    Fixed youtube dislike page and some css fixes in container

--- JS/ytdr.js
+++ JS/ytdr.js
@@ -2,12 +2,7 @@
 const outputArea = document.getElementById('output')
 const loader  = document.getElementById('loadingGif');
 
-// function randomColorPicker(){
-//     const hexNum = Math.floor(Math.random()*(256*256*256))
-//     const hexCode = "#"+ hexNum.toString(16);
-//     return hexCode;
-// }
-let dislikeReturn = async () => {
+async function dislikeReturn(){
     // let colorBgForOutputPTag = randomColorPicker();
     // console.log(colorBgForOutputPTag)
     outputArea.innerHTML =""
@@ -20,41 +15,43 @@ let dislikeReturn = async () => {
         outputArea.innerHTML=`<p class="invalidInputWarning invalidInputWarningPtag">Seems like you forgot to enter the id?`
         return;
     }
-let options = {
-        method:'GET',
-        headers:{
-            Accept: "text/html,application/xhtml+xml,application/xml;q=0.9",
-            Pragma: "no-cache",
-            "Cache-Control": "no-cache",
-            Connection: "keep-alive"
+    try{
+        let options = {
+            method:'GET',
+            headers:{
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9",
+                Pragma: "no-cache",
+                "Cache-Control": "no-cache",
+                Connection: "keep-alive"
+            }
         }
+        if(usrValElem.classList.contains("invalidInputWarning")){
+            usrValElem.classList.remove("invalidInputWarning")
+        }
+        let extractedId = usrVal.split("v=")[1] || usrVal;
+        console.log(extractedId)
+        let response = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${extractedId}`,options);
+        let result = await response.json();
+        const keysCount = Object.keys(result);
+        console.log(result);
+        loader.hidden = true;
+        if(result.status!=400){
+            outputArea.innerHTML = `
+                            <table>
+                            <tr><td>Total dislikes</td><td>Total Likes</td><td>Total Views</td></tr>
+                            <tr><td>${result.dislikes}</td><td>${result.likes}</td><td>${result.viewCount}</td></tr>
+        </table>
+        `;
+        }else{
+            outputArea.innerHTML=`<strong>Invalid ID!<strong>`
+        }
+    }catch(e){
+            loader.hidden=true;
+            outputArea.innerHTML=`Error: ${e}`
     }
-    if(usrValElem.classList.contains("invalidInputWarning")){
-        usrValElem.classList.remove("invalidInputWarning")
-    }
-    let extractedId = usrVal.split("v=")[1] || usrVal;
-    console.log(extractedId)
-let response = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${extractedId}`,options);
-let result = await response.json();
-const keysCount = Object.keys(result);
-console.log(keysCount);
-if(keysCount.length==7){}
-console.log(result);
-loader.hidden = true;
-outputArea.innerHTML = `
-                        <table>
-                        <tr><td>Total dislikes</td><td>Total Likes</td><td>Total Views</td></tr>
-                        <tr><td>${result.dislikes}</td><td>${result.likes}</td><td>${result.viewCount}</td></tr>
-                        </table>
-                        `;
-// let idOfDislikeContainer = document.getElementById("dislikeContainer");
-// idOfDislikeContainer.style.backgroundColor = colorBgForOutputPTag;
+
 }
-                    else{
-                        loader.hidden=true;
-                        outputArea.innerHTML=`<strong>Invalid ID!<strong>`
-                    }
-}
+                    
 
 document.getElementById('name').addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
