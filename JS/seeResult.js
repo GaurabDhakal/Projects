96c33a56d38ef9f8c9f8d34e57cbc5b6b150fcd7
Gaurab Dhakal <dhakalgaurab123@gmail.com>
2026-05-01 17:09:27 +0545

    ah shit here we go again: made a few enhancements

--- JS/seeResult.js
+++ JS/seeResult.js
@@ -1,54 +1,57 @@
-let inpArea = document.getElementById('inpArea');
-let goBack = document.getElementById('goBack');
-const historyArea = document.getElementById('historyMain');
-const historyInner = document.getElementById('history');
-const container = document.getElementById('container');
-let outputArea = document.getElementById('output');
-let historyWarning = document.getElementById('historyWarning');
+let inpArea = document.getElementById("inpArea");
+let goBack = document.getElementById("goBack");
+const historyArea = document.getElementById("historyMain");
+const historyInner = document.getElementById("history");
+const container = document.getElementById("container");
+let outputArea = document.getElementById("output");
+let historyWarning = document.getElementById("historyWarning");
 let keys = Object.keys(localStorage);
 
 async function chkResult() {
-    let loadingGif = document.getElementById('loadingGif');
-    const symbolN = document.getElementById('symbolN').value;
-    const dob = document.getElementById('dob').value;
-    outputArea.innerHTML = ""
+    let loadingGif = document.getElementById("loadingGif");
+    const symbolN = document.getElementById("symbolN").value;
+    const dob = document.getElementById("dob").value;
+    outputArea.innerHTML = "";
     if (symbolN.length === 0 || dob.length === 0) {
         outputArea.innerHTML = "Please fill up all details!";
     } else {
-
-
         inpArea.hidden = true;
-        loadingGif.hidden = false
+        loadingGif.hidden = false;
         const options = {
-            method: 'POST',
+            method: "POST",
             headers: {
-                "Accept": "application/json, text/plain, */*",
+                Accept: "application/json, text/plain, */*",
                 "Accept-Encoding": "gzip, deflate, br",
                 "Accept-Language": "en-US,en;q=0.9",
-                "Connection": "keep-alive",
+                Connection: "keep-alive",
                 "Content-Length": "47",
                 "Content-Type": "application/json",
-                "Host": "see.nicasiabank.com",
-                "Origin": "https://see.nicasiabank.com",
-                "Referer": "https://see.nicasiabank.com/see-result/check",
-                "Sec-Ch-Ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Microsoft Edge\";v=\"114\"",
+                Host: "see.nicasiabank.com",
+                Origin: "https://see.nicasiabank.com",
+                Referer: "https://see.nicasiabank.com/see-result/check",
+                "Sec-Ch-Ua":
+                    '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
                 "Sec-Ch-Ua-Mobile": "?0",
-                "Sec-Ch-Ua-Platform": "\"Windows\"",
+                "Sec-Ch-Ua-Platform": '"Windows"',
                 "Sec-Fetch-Dest": "empty",
                 "Sec-Fetch-Mode": "cors",
                 "Sec-Fetch-Site": "same-origin",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67",
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67",
                 "X-Requested-With": "XMLHttpRequest",
-                "X-Security-Request": "required"
+                "X-Security-Request": "required",
             },
             body: JSON.stringify({
                 symbol_number: symbolN,
-                dob: dob
-            })
+                dob: dob,
+            }),
         };
 
         try {
-            let response = await fetch('https://see.nicasiabank.com/api/see/get-result', options);
+            let response = await fetch(
+                "https://see.nicasiabank.com/api/see/get-result",
+                options,
+            );
             let final = await response.json();
             loadingGif.hidden = true;
             localStorage.setItem(`${symbolN}`, JSON.stringify(final));
@@ -69,22 +72,22 @@ async function chkResult() {
             <td>${final.data.gpa}</td>
             <td>${final.data.dob}</td>
         </tr>
-    </table>`
+    </table>`;
             goBack.hidden = false;
             console.log(final);
-        }
-        catch (error) {
+        } catch (error) {
             goBack.hidden = false;
             loadingGif.hidden = true;
-            outputArea.innerHTML = "Pardon us! The API has been closed. Please try again later"
+            outputArea.innerHTML =
+                "Pardon us! The API has been closed. Please try again later";
             console.error(error);
-        }}
+        }
+    }
 }
 function goBackk() {
     inpArea.hidden = false;
     goBack.hidden = true;
-    outputArea.innerHTML = ""
-
+    outputArea.innerHTML = "";
 }
 function showHistory() {
     let i = 1;
@@ -94,8 +97,12 @@ function showHistory() {
         let value = localStorage.getItem(key);
         try {
             let parsedValue = JSON.parse(value);
-            if (typeof parsedValue === "object" && parsedValue !== null && !Array.isArray(parsedValue)) {
-                historyWarning.innerHTML = ""
+            if (
+                typeof parsedValue === "object" &&
+                parsedValue !== null &&
+                !Array.isArray(parsedValue)
+            ) {
+                historyWarning.innerHTML = "";
                 historyInner.innerHTML += `<p>
                 ${i}. Symbol Number: ${parsedValue.data.symbol_number}
                     GPA: ${parsedValue.data.gpa}
@@ -105,18 +112,16 @@ function showHistory() {
         } catch (error) {
             console.error("Error parsing value for key:", key, error);
         }
-
     }
 }
 
 function goBackFromHistoryPage() {
-    container.hidden = false
+    container.hidden = false;
     historyArea.hidden = true;
-    historyInner.innerHTML = ''
-
+    historyInner.innerHTML = "";
 }
-document.getElementById('dob').addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
+document.getElementById("dob").addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
         chkResult();
     }
 });
--- JS/weather.js
+++ JS/weather.js
@@ -1,92 +1,103 @@
 const usrForm = document.querySelector(".weatherForm");
 const usrInp = document.querySelector(".usrInp");
 const btnSubmit = document.querySelector(".mainBtn");
-const resultDisplayAreaParent = document.querySelector(".resultDisplayAreaMain")
-const resultDisplayArea = document.querySelector(".resultDisplayArea")
+const resultDisplayAreaParent = document.querySelector(
+    ".resultDisplayAreaMain",
+);
+const resultDisplayArea = document.querySelector(".resultDisplayArea");
 const containerMain = document.querySelector(".container");
 const loadingGif = document.querySelector("#loadingGif");
 const api = "426d2f090c061430c02a4ea9ba629bff";
 
-usrForm.addEventListener("submit",async event =>{
+let cache = {};
+
+usrForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const city = usrInp.value;
-    if(city){
-    try{
-        loadingGif.hidden=false;
-        resultDisplayAreaParent.hidden=true;
-        const weatherData = await weatherInfo(city);
-        displayWeather(weatherData);
-    }catch(error){
-        displayError(error);
-    }
-    }else{
+    if (city) {
+        try {
+            loadingGif.hidden = false;
+            resultDisplayAreaParent.hidden = true;
+            const weatherData = await weatherInfo(city);
+            displayWeather(weatherData);
+        } catch (error) {
+            displayError(error);
+        }
+    } else {
         displayError("Enter something first!");
     }
 });
 
-async function weatherInfo(usrVal){
+async function weatherInfo(usrVal) {
+    if (cache[usrVal.toLowerCase()]) {
+        console.log("cache hit");
+        return cache[usrVal.toLowerCase()];
+    }
     const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${usrVal}&appid=${api}`;
-    let response = (await fetch(api_url));
-    if(!response.ok){
+    let response = await fetch(api_url);
+    if (!response.ok) {
         throw new Error("Unable to fetch data!");
     }
-    return response.json();
+    const data = response.json();
+    cache[usrVal.toLowerCase()] = data;
+    return data;
 }
-function displayWeather(weatherData){
+function displayWeather(weatherData) {
     resultDisplayAreaParent.hidden = false;
-        const {name:city,
-                main:{temp,humidity},
-                weather:[{description,id}]
-            } = weatherData;
-        let weatherTempr = document.createElement("p");
-        let weatherCity = document.createElement("p");
-        let weatherDescription = document.createElement("p");
-        let weatherHumidity = document.createElement("p");
-        let weatherEmoji = document.createElement("p");
-        window.document.title = `${(temp-273.15).toFixed(2)}°C - ${city}`
-        weatherTempr.innerHTML = `<span class="tempr">Temperature: ${(temp-273.15).toFixed(2)}°C</span>`;
-        weatherCity.innerHTML = `<span class="city">${city}</span>`;
-        let finalDescription = ((description)).charAt(0).toUpperCase()+((description)).slice(1);
-        weatherDescription.textContent = `Description: ${finalDescription}`;
-        weatherHumidity.textContent = `Humidity: ${humidity}%`;
-        weatherEmoji.textContent = weatherEmojiDisplay(id);
-        
-        weatherEmoji.classList.add("weatherEmoji");
-        loadingGif.hidden=true;
+    const {
+        name: city,
+        main: { temp, humidity },
+        weather: [{ description, id }],
+    } = weatherData;
+    let weatherTempr = document.createElement("p");
+    let weatherCity = document.createElement("p");
+    let weatherDescription = document.createElement("p");
+    let weatherHumidity = document.createElement("p");
+    let weatherEmoji = document.createElement("p");
+    globalThis.document.title = `${(temp - 273.15).toFixed(2)}°C - ${city}`;
+    weatherTempr.innerHTML = `<span class="tempr">Temperature: ${(temp - 273.15).toFixed(2)}°C</span>`;
+    weatherCity.innerHTML = `<span class="city">${city}</span>`;
+    let finalDescription =
+        description.charAt(0).toUpperCase() + description.slice(1);
+    weatherDescription.textContent = `Description: ${finalDescription}`;
+    weatherHumidity.textContent = `Humidity: ${humidity}%`;
+    weatherEmoji.textContent = weatherEmojiDisplay(id);
 
-        resultDisplayAreaParent.hidden=false;
-        resultDisplayArea.textContent = "";
-        resultDisplayArea.appendChild(weatherCity);
-        resultDisplayArea.appendChild(weatherTempr);
-        resultDisplayArea.appendChild(weatherHumidity);
-        resultDisplayArea.appendChild(weatherDescription);
-        resultDisplayArea.appendChild(weatherEmoji);
-        console.log(weatherData);
+    weatherEmoji.classList.add("weatherEmoji");
+    loadingGif.hidden = true;
+
+    resultDisplayAreaParent.hidden = false;
+    resultDisplayArea.textContent = "";
+    resultDisplayArea.appendChild(weatherCity);
+    resultDisplayArea.appendChild(weatherTempr);
+    resultDisplayArea.appendChild(weatherHumidity);
+    resultDisplayArea.appendChild(weatherDescription);
+    resultDisplayArea.appendChild(weatherEmoji);
 }
-function displayError(error){
+function displayError(error) {
     let createElem = document.createElement("p");
     createElem.textContent = error;
-    createElem.classList.add("errorMessage")
-    resultDisplayArea.textContent="";
-    loadingGif.hidden=true;
-    resultDisplayAreaParent.hidden=false;
+    createElem.classList.add("errorMessage");
+    resultDisplayArea.textContent = "";
+    loadingGif.hidden = true;
+    resultDisplayAreaParent.hidden = false;
     resultDisplayArea.appendChild(createElem);
 }
-function weatherEmojiDisplay(id){
-    switch(true){
-    case (id >= 200  && id<300):
-        return "⛈️";
-    case (id >= 300  && id<400):
-        return "⛈️";
-    case (id >= 500  && id<600):
-        return "🌧️";
-    case (id >=600  && id<700):
-        return "❄️";
-    case (id >= 700  && id<800):
-        return "🌫️";
-    case (id ===800):
-        return "☀️";
-    case (id>800&&id<=804):
-        return "☁️";
+function weatherEmojiDisplay(id) {
+    switch (true) {
+        case id >= 200 && id < 300:
+            return "⛈️";
+        case id >= 300 && id < 400:
+            return "⛈️";
+        case id >= 500 && id < 600:
+            return "🌧️";
+        case id >= 600 && id < 700:
+            return "❄️";
+        case id >= 700 && id < 800:
+            return "🌫️";
+        case id === 800:
+            return "☀️";
+        case id > 800 && id <= 804:
+            return "☁️";
     }
 }
